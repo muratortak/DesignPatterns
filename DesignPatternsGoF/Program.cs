7826921de16f0d9378e116c002b443f79fd25f20
unknown <murat.ortak35@hotmail.com>
2019-11-04 14:53:07 -0500

    First Commit

---
+++ DesignPatternsGoF/Program.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsGoF
+{
+    // When piecewise object construction is complicated,
+    // provide an API for doing it succinctly.
+    #region Builder Pattern
+    //public class HtmlElement
+    //{
+    //    public string Name, Text;
+    //    public List<HtmlElement> Elements = new List<HtmlElement>();
+    //    private const int indentSize = 2;
+
+    //    public HtmlElement()
+    //    {
+
+    //    }
+    //    public HtmlElement(string name, string text)
+    //    {
+    //        if(name == null)
+    //        {
+    //            throw new ArgumentNullException(paramName: nameof(name));
+    //        }
+
+    //        if (text == null)
+    //        {
+    //            throw new ArgumentNullException(paramName: nameof(text));
+    //        }
+
+    //        this.Name = name;
+    //        this.Text = text;
+    //    }
+
+    //    private string ToStringImpl(int indent)
+    //    {
+    //        var sb = new StringBuilder();
+    //        var i = new string(' ', indentSize * indent);
+    //        sb.AppendLine($"{i}<{Name}>");
+
+    //        if(!string.IsNullOrWhiteSpace(Text))
+    //        {
+    //            sb.Append(new string(' ', indentSize * indent + 1));
+    //            sb.AppendLine(Text);
+    //        }
+
+    //        foreach (var e in Elements)
+    //        {
+    //            sb.Append(e.ToStringImpl(indent + 1));
+    //        }
+
+    //        sb.AppendLine($"{i}</{Name}>");
+
+    //        return sb.ToString();
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return ToStringImpl(0);
+    //    }
+    //}
+
+    //public class HtmlBuilder
+    //{
+
+    //    private readonly string rootName;
+    //    HtmlElement root = new HtmlElement();
+
+    //    public HtmlBuilder(string rootName)
+    //    {
+    //        this.rootName = rootName;
+    //        root.Name = rootName;
+    //    }
+
+    //    public HtmlBuilder AddChild(string childName, string childText)
+    //    {
+    //        var e = new HtmlElement(childName, childText);
+    //        root.Elements.Add(e);
+    //        return this;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return root.ToString();
+    //    }
+
+    //    public void Clear()
+    //    {
+    //        root = new HtmlElement { Name = rootName };
+    //    }
+    //}
+
+    #endregion
+
+    #region Fluent Builder Inheritance with Recursive Generics
+    //public class Person
+    //{
+    //    public string Name;
+    //    public string Position;
+
+    //    public class Builder : PersonJobBuilder<Builder>
+    //    {
+
+    //    }
+
+    //    public static Builder New => new Builder();
+    //    public override string ToString()
+    //    {
+    //        return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+    //    }
+    //}
+
+    //public abstract class PersonBuilder
+    //{
+    //    protected Person person = new Person();
+    //    public Person Build()
+    //    {
+    //        return person;
+    //    }
+    //}
+
+    //// class Foo : Bar<Foo>
+    //public class PersonInfoBuilder<SELF> : PersonBuilder where SELF : PersonInfoBuilder<SELF>
+    //{
+
+    //    public SELF Called(string name)
+    //    {
+    //        person.Name = name;
+    //        return (SELF)this;
+    //    }
+    //}
+
+    //public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>> where SELF : PersonJobBuilder<SELF>
+    //{
+    //    public SELF WorksAsA(string position)
+    //    {
+    //        person.Position = position;
+    //        return (SELF) this;
+    //    }
+    //}
+    #endregion
+
+    #region Faceted Builder
+    //public class Person
+    //{
+    //    public string StreetAddress, Postcode, City;
+
+    //    public string CompanyName, Position;
+    //    public int AnnualIncome;
+
+    //    public override string ToString()
+    //    {
+    //        return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}," +
+    //            $"{nameof(Position)}: {Position}, { nameof(AnnualIncome)}: {AnnualIncome}";
+    //    }
+    //}
+
+    //public class PersonBuilder // facade
+    //{
+    //    // reference!
+    //    protected Person person = new Person();
+    //    public PersonJobBuilder Works => new PersonJobBuilder(person);
+    //    public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+    //    public static implicit operator Person(PersonBuilder pb)
+    //    {
+    //        return pb.person;
+    //    }
+    //}
+
+    //public class PersonAddressBuilder : PersonBuilder
+    //{
+    //    // might not work with a value type!
+    //    public PersonAddressBuilder(Person person)
+    //    {
+    //        this.person = person;
+    //    }
+
+    //    public PersonAddressBuilder At(string streetAddress)
+    //    {
+    //        person.StreetAddress = streetAddress;
+    //        return this;
+    //    }
+
+    //    public PersonAddressBuilder WithPostCode(string postcode)
+    //    {
+    //        person.Postcode = postcode;
+    //        return this;
+    //    }
+
+    //    public PersonAddressBuilder In(string city)
+    //    {
+    //        person.City = city;
+    //        return this;
+    //    }
+    //}
+
+    //public class PersonJobBuilder : PersonBuilder
+    //{
+    //    public PersonJobBuilder(Person person)
+    //    {
+    //        this.person = person;
+    //    }
+
+    //    public PersonJobBuilder At(string companyName)
+    //    {
+    //        person.CompanyName = companyName;
+    //        return this;
+    //    }
+
+    //    public PersonJobBuilder AsA(string position)
+    //    {
+    //        person.Position = position;
+    //        return this;
+    //    }
+
+    //    public PersonJobBuilder Earning(int amount)
+    //    {
+    //        person.AnnualIncome = amount;
+    //        return this;
+    //    }
+    //}
+    #endregion
+
+    #region Excersise
+    public class Code
+    {
+        public string className;
+        public string propName;
+        public string propType;
+
+        public Code()
+        {
+
+        }
+
+        public Code(string classname, string propname, string propType)
+        {
+            if(classname == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(classname));
+            }
+            if (propname == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(propname));
+            }
+
+            this.className = classname;
+            this.propName = propname;
+            this.propType = propType;
+        }
+    }
+
+    public class CodeBuilder
+    {
+        Code code = new Code();
+
+        public CodeBuilder()
+        {
+
+        }
+
+        public CodeBuilder(string className)
+        {
+            code.className = className;
+        }
+
+        public CodeBuilder addField(string propName, string propType)
+        {
+            code.propName = propName;
+            code.propType = propType;
+            return this;
+        }
+    }
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Builder Pattern Client
+            //var hello = "hello";
+            //var sb = new StringBuilder();
+            //sb.Append("<p>");
+            //sb.Append(hello);
+            //sb.Append("</p>");
+            //Console.WriteLine(sb);
+
+            //var words = new[] { "hello", "world" };
+            //sb.Clear();
+            //sb.Append("<ul>");
+            //foreach (var word in words)
+            //{
+            //    sb.AppendFormat("<li>{0}</li>", word);
+            //}
+            //sb.Append("</ul>");
+            //Console.WriteLine(sb);
+
+            //var builder = new HtmlBuilder("ul");
+            //builder.AddChild("li", "hello").AddChild("li", "world");
+            //Console.WriteLine(builder.ToString());
+            #endregion
+
+            #region Fluent Builder Inheritance with Recursive Generics Client
+            //var person = Person.New
+            //    .Called("Dimitri")
+            //    .WorksAsA("quant")
+            //    .Build();
+            //Console.WriteLine(person);
+            #endregion
+
+            #region Faceted Builder Client
+            //var pb = new PersonBuilder();
+            //Person person = pb
+            //    .Lives.At("123 London Road")
+            //        .In("London")
+            //        .WithPostCode("SW12C")
+            //    .Works.At("Fabrikam")
+            //        .AsA("Engineer")
+            //        .Earning(123000);
+            //Console.WriteLine(person);
+            #endregion
+        }
+    }
+
+   
+
+}
---
+++ Factories/Program.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+/**
+* Why use Factories:
+*  Object creation logic becomes too convoluted
+*  Constructor is not descriptive
+*      Name mandated by name of containing type
+*      Cannot overload with same sets of arguments with different names
+*      Can turn into 'optional parameter hell' 
+*  Object creation (non-piecewise, unlike Builder) can be outsourced to
+*      A separate function (Factory Method)
+*      That may exist in a separate class (Factory)
+*      Can create hierarchy of factories with Abstract Factory
+* 
+* Factory: 
+*  A component responsible solely for the wholesale (not piecewise) creation of objects.
+* **/
+namespace Factories
+{
+    #region ConcreteFactory
+    //public enum CoordinateSystem
+    //{
+    //    Cartesian,
+    //    Polar
+    //}
+    public class Point
+    {
+        private double x,y;
+        /// <summary>
+        /// Initializes a point from EITHER Cartesian or Polar.
+        /// PROBLEM HERE:
+        ///     There is too much going on with the Ctor
+        ///     Constructor signature is ambigous in terms of which paramter is assigned to a or b
+        /// </summary>
+        /// <param name="a">x if Cartesian, rho if polar</param>
+        /// <param name="b"></param>
+        /// <param name="system"></param>
+        //public Point(double a, double b, 
+        //    CoordinateSystem system = CoordinateSystem.Cartesian)
+        //{
+        //    switch (system)
+        //    {
+        //        case CoordinateSystem.Cartesian:
+        //            x = a;
+        //            y = b;
+        //            break;
+        //        case CoordinateSystem.Polar:
+        //            x = a * Math.Cos(b);
+        //            y = b * Math.Sin(b);
+        //            break;
+        //    }
+        //}
+
+        // factory method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+        /// <summary>
+        /// Advantage of Factory Method:
+        ///     You get to have an overload with same set of arguments. But they have DIFFERENT, descriptive name so API tells you
+        ///         what arguments you are providing.
+        ///     Names of the Factory Methods are also UNIQUE.
+        ///    
+        /// OVERALL THIS IS AN API IMPROVEMENT.
+        ///     
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
+        // Arrow makes it a property. Everytime you try to access the Point Origin, you will be getting a new coor with 0,0.
+        //and you would access it in the main var origin = Point.Origin; If you want to instantiate a new object any time anybody ask for something
+        //then a property is a good method.
+        public static Point Origin => new Point(0, 0);
+
+        // This is just a field. You initialize a static field once. It is avaliable everywhere  
+        public static Point Origin2 = new Point(0, 0); // better
+
+        //public static PointFactory Factory => new PointFactory();
+        // If you make PointFactory make the class and its method Static. Better approach!
+        //public class PointFactory
+        public static class Factory
+        {
+            //public Point NewCartesianPoint(double x, double y)
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            //public Point NewPolarPoint(double rho, double theta)
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+
+    }
+
+    // FOR SMALL, SIMPLE OBJECTS FACTORY METHOD WITH CTOR IS OKAY. BUT IF YOU NEEDED A FACTORY, IT IS BETTER TO HAVE FACTORY CLASS.
+
+    //public class PointFactory
+    //{
+    //    public static Point NewCartesianPoint(double x, double y)
+    //    {
+    //        return new Point(x, y);
+    //    }
+
+    //    public static Point NewPolarPoint(double rho, double theta)
+    //    {
+    //        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+    //    }
+    //}
+
+    #endregion
+
+    #region AbstractFactory
+    // Only real use of Abstract Factory is to give out Abstract Object as opposed to Concrete Object.
+    // In Abstract you are not returning the type you are creating. You are returning Abstract classes or Interfaces.
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+
+    // Internal is only used in the class so they won't be given out to client but no factory methods won't return tea or cofee but will return IHotDrink.
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("This tea is nice.");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("This coffee is nice.");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Put in a tea bag, boil water, pout {amount} ml, add lemon, enjoy!");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Grind some beans, boil water, pout {amount} ml, add cream and sugar, enjoy!");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        //Enum violates Open-Close principle. You have to add a new type of drink. That means changing the code.
+        //public enum AvailableDrink
+        //{
+        //    Coffee, Tea
+        //}
+
+        //private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        //public HotDrinkMachine()
+        //{
+        //    foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        //    {
+        //        var factory = (IHotDrinkFactory)Activator.CreateInstance(
+        //            Type.GetType("Factories." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
+        //            );
+        //        factories.Add(drink, factory);
+        //    }
+        //}
+
+        //public IHotDrink MakeDrink( AvailableDrink drink, int amount)
+        //{
+        //    return factories[drink].Prepare(amount);
+        //}
+
+        private List<Tuple<string, IHotDrinkFactory>> factories = new List<Tuple<string, IHotDrinkFactory>>();
+        public HotDrinkMachine()
+        {
+            foreach(var t in typeof(HotDrinkMachine).Assembly.GetTypes())
+            {
+                if(typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+                {
+                    factories.Add(Tuple.Create(
+                        t.Name.Replace("Factory", string.Empty),
+                        (IHotDrinkFactory)Activator.CreateInstance(t)
+                        ));
+                }
+            }
+        }
+
+        public IHotDrink MakeDrink()
+        {
+            Console.WriteLine("Available Drinks: ");
+            for(var index = 0; index < factories.Count; index++)
+            {
+                var tuple = factories[index];
+                Console.WriteLine($"{index}: {tuple.Item1}");
+            }
+
+            while (true)
+            {
+                string s;
+                if ((s = Console.ReadLine()) != null && int.TryParse(s, out int i) && i >= 0 && i < factories.Count)
+                {
+                    Console.WriteLine("Specify amount: ");
+                    s = Console.ReadLine();
+                    if(s != null && int.TryParse(s, out int amount) && amount > 0)
+                    {
+                        return factories[i].Item2.Prepare(amount);
+                    }
+                }
+                Console.WriteLine("Incorrect input, try again!");
+            }
+        }
+
+    }
+
+    #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Factories Project!");
+            #region ConcreteFactoryClient
+            //var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            //var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
+            //var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
+            //Console.WriteLine(point);
+            #endregion
+
+            #region AbstractFactoryClient
+            var machine = new HotDrinkMachine();
+            //var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+            //drink.Consume();
+            var drink = machine.MakeDrink();
+            drink.Consume();
+            #endregion
+        }
+    }
+}
